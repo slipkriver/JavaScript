6fe87154d151dce4597a399719580855eb9bca4a
Elias Rivera <slipkriver@gmail.com>
2020-07-16 12:51:20 -0500

    Creando el módulo de peticiones a través de HTTP

--- 08 - Callback-Promises/src/index.js
+++ 08 - Callback-Promises/src/index.js
@@ -1,7 +1,18 @@
 
-import { obtenerHeroesArr } from './js/await';
+// import { obtenerHeroesArr, obtenerHeroesAwait } from './js/await';
+import { heroeAwait, heroeCiclo } from './js/await';
 import './styles.css';
 
-console.time('await');
-obtenerHeroesArr().then(heroe => console.table(heroe));
-console.timeEnd('await');
+// console.time('await');
+// obtenerHeroesArr().then(heroe => console.table(heroe));
+// console.timeEnd('await');
+
+// console.time('await');
+// obtenerHeroesAwait('capi').
+//     then(heroe => {
+//         console.log(heroe)
+//         console.timeEnd('await');
+//     }).catch(console.warn);
+
+heroeCiclo()
+heroeAwait('iron');
--- 08 - Callback-Promises/src/js/await.js
+++ 08 - Callback-Promises/src/js/await.js
@@ -1,12 +1,52 @@
 import { buscarHeroeAsync, buscarHeroe } from "./promises";
 
 const heroesIds = ['capi', 'iron', 'spidy'];
+const heroesPromesas = heroesIds.map( buscarHeroe );
 
 export const obtenerHeroesArr = async() => {
-    const heoresArr = [];
-    for (const id of heroesIds){
-        heoresArr.push(buscarHeroe(id));
+
+    return await Promise.all(heroesIds.map(buscarHeroe));
+
+//     const heoresArr = [];
+//     for (const id of heroesIds){
+//         heoresArr.push(buscarHeroe(id));
+//     }
+
+//     return await Promise.all(heoresArr);
+}
+
+export const obtenerHeroesAwait = async(id) => {
+    
+    try{
+        const heroe = await buscarHeroeAsync(id);
+        return heroe;
+
     }
 
-    return await Promise.all(heoresArr);
+    catch(err){
+        console.log('-- Catch() --');
+        // throw err;
+        return{
+            nombre: 'Sin nombre',
+            poder: 'Sin poder'
+        };
+    }
+}
+
+export const heroeCiclo = async() => {
+    console.time('Heroes Ciclo');
+
+    for await(const heroe of heroesPromesas){
+        console.log(heroe);
+    }
+    console.timeEnd('Heroes Ciclo');
+}
+
+export const heroeAwait = async(id) => {
+    if((await buscarHeroeAsync(id)).nombre === 'Ironman'){
+        console.log('Es el mas millonario');
+    }
+    else{
+        console.log('Y es exitoso');
+    }
 }
